530ca01c2f4fa4cc4851315cddb54e2505cbaf52
Alexander Wilkerson <alex@uncorrected.com>
2018-06-01 14:40:42 -0500

    testing

--- main.py
+++ main.py
@@ -608,7 +608,7 @@ class Leaderboard(object):
         self.leaderboard_data = req.content
 
         #if (user < 1 or user > to_int_32(self.leaderboard_data, 75)):
-        if (user < 1 or user 200000 > to_int_32(self.leaderboard_data, 75)):
+        if (user < 1 or user > 200000):
             return None
 
         byte_pos = 83
