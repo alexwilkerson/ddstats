ece16cc5a91d92e84c50408aab37f90b85b967f5
Alexander Wilkerson <alex@uncorrected.com>
2018-06-18 02:02:21 -0500

    oops bug

--- main.py
+++ main.py
@@ -526,7 +526,7 @@ def get_game_enemies_alive(game_number):
 def create_game():
     data = request.get_json()
 
-    if data["player_id"] == -1:
+    if "player_id" not in data:
         return jsonify({'message': 'Some kind of error occurred.'})
 
     if 'playerName' in data:
