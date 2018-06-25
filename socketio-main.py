229b6c3da5790fa0bbc9d92197f4f1800dfac775
Alexander Wilkerson <alex@uncorrected.com>
2018-06-24 21:15:22 -0500

    reconfiguring socketio

---
+++ socketio-main.py
@@ -0,0 +1,112 @@
+import os
+from datetime import datetime
+from flask import Flask, request, jsonify, Response, url_for
+from flask import render_template
+from flask_socketio import SocketIO, emit
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import and_
+from flask_bower import Bower
+from flask_cors import CORS
+from time_ago import time_ago
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+Bower(app)
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI') \
+    or 'sqlite:////Users/alex/code/ddstats/app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSON_SORT_KEYS'] = False
+
+# jinja filters
+app.jinja_env.filters['time_ago'] = time_ago
+
+db = SQLAlchemy(app)
+
+socketio = SocketIO(app)
+
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    player_id = db.Column(db.Integer, nullable=False)
+    granularity = db.Column(db.Integer, nullable=False)
+    game_time = db.Column(db.Float, nullable=False)
+    death_type = db.Column(db.Integer, nullable=False)
+    gems = db.Column(db.Integer, nullable=False)
+    homing_daggers = db.Column(db.Integer, nullable=False)
+    daggers_fired = db.Column(db.Integer, nullable=False)
+    daggers_hit = db.Column(db.Integer, nullable=False)
+    enemies_alive = db.Column(db.Integer, nullable=False)
+    enemies_killed = db.Column(db.Integer, nullable=False)
+    time_stamp = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    replay_player_id = db.Column(db.Integer, default=0, nullable=False)
+    survival_hash = db.Column(db.String(32))
+    version = db.Column(db.String(16))
+
+
+class State(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    game_id = db.Column(db.Integer, db.ForeignKey('game.id'), nullable=False)
+    game_time = db.Column(db.Float, nullable=False)
+    gems = db.Column(db.Integer, nullable=False)
+    homing_daggers = db.Column(db.Integer, nullable=False)
+    daggers_hit = db.Column(db.Integer, nullable=False)
+    daggers_fired = db.Column(db.Integer, nullable=False)
+    enemies_alive = db.Column(db.Integer, nullable=False)
+    enemies_killed = db.Column(db.Integer, nullable=False)
+
+    @property
+    def serialize(self):
+        return {
+                'game_time': self.game_time,
+                'gems': self.gems,
+                'homing_daggers': self.homing_daggers,
+                'daggers_hit': self.daggers_hit,
+                'daggers_fired': self.daggers_fired,
+                'enemies_alive': self.enemies_alive,
+                'enemies_killed': self.enemies_killed
+               }
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    username = db.Column(db.String(128), index=True, nullable=False)
+
+
+########################################################
+#                    socketio stuff                    #
+########################################################
+
+@app.route('/socketio_test')
+def show_socketio_test():
+    return render_template('socketio_test.html')
+
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    emit('my response', {'data': 'Connected'}, broadcast=True)
+
+########################################################
+#                  end socketio stuff                  #
+########################################################
+
+
+# these two functions append the timestamp to static files so the flush
+# correctly when updated.
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path, endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=7666, debug=True)
